e39e1f95bcc0ab2bbc80b97fd8d7f16f967805ee
Charm-YY <127134319+Charm-YY@users.noreply.github.com>
2023-03-27 03:07:04 +0900

    event함수 공부
    event함수 공부

---
+++ eventListenr.js
@@ -0,0 +1,23 @@
+const $title = document.querySelector('.hello:first-child h1'); // class hello 에서 처음 h1 선택
+
+const $hello = document.querySelector('.hello h1:nth-of-type(2)'); // class hello 에서 두번째 h1 선택
+
+function handleTitleClick($element, color){ // 클릭 했을때 파란색으로 바꿔줘
+    $element.style.color = color;
+}
+
+function handleMouseEnter($element, name){ // 마우스를 가져가면 텍스트를 바꿔줘
+    $element.innerHTML = `Hi, ${name}`;
+}
+
+function handleMouseLeave($element, name){ // 마우스를 떼면 텍스트를 바꿔줘
+    $element.innerHTML = `Bye, ${name}`;
+}
+
+$title.addEventListener('click', ()=> handleTitleClick($title, 'red'));
+$title.addEventListener('mouseenter', () => handleMouseEnter($title, 'JavaScript'));
+$title.addEventListener('mouseleave', () => handleMouseLeave($title, 'JavaScript'));
+
+$hello.addEventListener('click', ()=> handleTitleClick($hello, 'blue'));
+$hello.addEventListener('mouseenter', () => handleMouseEnter($hello, 'CSS'));
+$hello.addEventListener('mouseleave', () => handleMouseLeave($hello, 'CSS'));
